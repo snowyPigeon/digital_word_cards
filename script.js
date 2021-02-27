9b7bdc86ff48173b0fea0e5b78c16cd4cf306284
Snowy Pigeon <7503186+snowyPigeon@users.noreply.github.com>
2021-02-27 22:42:28 +0000

    Add files via upload
    Initial upload of skeleton files. Presently the check and next functions do not fully work.

---
+++ script.js
@@ -0,0 +1,22 @@
+let pictures = ["bell", "chair", "door", "egg", "fish"];
+
+function nextCard(){
+	let i,
+	let (i = 0; i < pictures.length; i++){ //loop which moves the wordless image on with click of 'next' button
+		$("#card_pic").html("<img src='" + pictures[i] + ".jpg'/>");
+	}
+}
+
+function checkCard(){
+	if (input.value = pictures[i])
+		{
+			document.getElementById('message').value = "Correct!";
+			$("#card_wpic").html("<img src='" + pictures[i] + "_word" +".jpg'/>"); //moves word and pic img on if input test correct
+		}
+		else
+		{
+			$("#card_wpic").html("<img src=''/>");
+			document.getElementById('message').value = "Your answer was wrong. Please have another try.";
+		}
+		
+}
