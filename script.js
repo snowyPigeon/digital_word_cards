8499b236b3e570b7a42341f3320c63d3bc5fd680
Jash Jain <jashjain21@gmail.com>
2021-02-28 16:06:57 +0530

    Added UI Added functions

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-let pictures = ["bell", "chair", "door", "egg", "fish"];
+let pictures = ["bell", "chair", "door", "egg", "fish"],index=0;
+// print(pictures.length)
+// function nextCard(){
+// 	//let i,
+// 	let(i = 0; i < pictures.length; i++){ //loop which moves the wordless image on with click of 'next' button
+// 		$("#card_pic").html("<img src='" + pictures[i] + ".jpg'/>");
+// 	}
+// }
+function nextImage() {
+    index+=1;
+    if (index > pictures.length - 1) {
+     index = 0;
+    }
+	console.log(index);
+	//$("#card_pic").html("<img src='" + pictures[index] + ".jpg'/>");
+	document.getElementById('card_pic').src= pictures[index] + ".jpg"
+	document.getElementById("card_pic").width = "300";
+	document.getElementById("card_pic").height = "300";
+	document.getElementById('card_wpic').src= "logo.jpg"
+	document.getElementById("card_wpic").width = "300";
+	document.getElementById("card_wpic").height = "300";
+    //document.getElementById('imgsrc').src = images[index];
+	document.getElementById('inputbox').value=''
+	document.getElementById('message').value = "";
 
-function nextCard(){
-	let i,
-	let (i = 0; i < pictures.length; i++){ //loop which moves the wordless image on with click of 'next' button
-		$("#card_pic").html("<img src='" + pictures[i] + ".jpg'/>");
-	}
 }
-
 function checkCard(){
-	if (document.getElementById('inputbox').value == pictures[i])
+		if (document.getElementById('inputbox').value == pictures[index])
 		{
 			document.getElementById('message').value = "Correct!";
-			$("#card_wpic").html("<img src='" + pictures[i] + "_word" +".jpg'/>"); //moves word and pic img on if input test correct
+			//console.log("<img src='" + pictures[index] + "_word" +".jpg'/>");
+			document.getElementById('card_wpic').src= pictures[index] + "_word.jpg"
+			document.getElementById("card_wpic").width = "300";
+			document.getElementById("card_wpic").height = "300";
+			//$("#card_wpic").html("<img src='" + pictures[index] + "_word.jpg'/>");
+			//$("#card_wpic").html("<img src='" + pictures[index] + "_word" +".jpg'/>"); //moves word and pic img on if input test correct
 		}
 		else
 		{
-			$("#card_wpic").html("<img src=''/>");
-			document.getElementById('message').value = "Your answer was wrong. Please have another try.";
+			document.getElementById('card_wpic').src= "logo.jpg"
+			document.getElementById("card_wpic").width = "300";
+			document.getElementById("card_wpic").height = "300";
+			//$("#card_wpic").html("<img src='logo.jpg'/>");
+			document.getElementById('message').value = "Wrong Ans.Have another try.";
 		}
-		
 }
